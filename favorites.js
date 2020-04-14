429d297be635689404a82c234523be1e887f1336
clemson tigress <anreeves83@gmail.com>
2020-04-14 16:02:07 -0400

    Favorites page - display recipe images and titles

--- favorites.js
+++ favorites.js
@@ -3,52 +3,55 @@
 $(document).ready(function() {
 
 
-    var favorites = [215435,323420,116679,121545];
-    console.log(favorites.join());
+    var favorites = [215435,323420,116679,121545,485365,74172,74202,94640,107878,]; //this will be array from local Storage - get more recipes
+    //console.log(favorites.join());
     
+
+    $('#searchBtn').on('click', showFavorites() ) //change to on load
+       
     
-    $('#button').on('click', function() {
-    
-        //var recipe = {
-           // "id1" :123, "id2" : 345, "id3" : 678
-         //}
-         
-         //favorites.push(recipe.id3);
-    
-         //localStorage.setItem("favorites", JSON.stringify(favorites));
-        //apiCall()
-    
-    })
-    
-    //$('#button2').on('click', function() {
-        //var storedFavorites = JSON.parse(localStorage.getItem("favorites"));
-        //$("#display").text(storedFavorites); })
-    
-    
-    
-       //function apiCall() {
+       function showFavorites() {
             var recipeId = favorites.join()
-            
     
-             var apiKey = "08f60c4364f74e17b4143c40fa0cff70"
-             var queryURL = `https://api.spoonacular.com/recipes/informationBulk?ids=${recipeId}&apiKey=${apiKey}`;
-             //var queryURL = `https://api.spoonacular.com/recipes/search?query=cheese&number=4&apiKey=${apiKey}`;
-          
+            var apiKey = "08f60c4364f74e17b4143c40fa0cff70"
+            var queryURL = `https://api.spoonacular.com/recipes/informationBulk?ids=${recipeId}&apiKey=${apiKey}`;
+                  
              $.ajax({
               url: queryURL,
                method: "GET"
            
              }).then(function(response) {
-    
                 console.log(response)
                 console.log(queryURL)
+                
     
                 //display response data in html
-                //var image = response.image
-                //$('#recipeImg').src(image)
-                //var title = response.title
-               // $('#title').text(title)
-    
+
+                var titleIds = ["title1", "title2", "title3", "title4","title5","title6","title7","title8","title9"]
+                var imageIds = ["image1", "image2","image3","image4","image5","image6","image7","image8","image9"]
+                var recipeResults = response
+                console.log(recipeResults[0].image)
+
+                    for(i=0; i < recipeResults.length; i++) {
+                    
+                        for(i=0; i < titleIds.length; i++) {
+
+                            var titleSelector = '#' + titleIds[i]
+                
+                            $(titleSelector).text(recipeResults[i].title)
+                        }
+                        
+                        for(i=0; i < imageIds.length; i++) {
+
+                            var imageSelector = '#' + imageIds[i]
+
+                            $(imageSelector).attr("src", recipeResults[i].image);
+                        }
+               
+                    }
+
+                
+
              })
     
             }
