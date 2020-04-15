91f51ff6d88cd12abb62a9da60c9c8d92e91c632
Danielle Beaty <daniellerbeaty@gmail.com>
2020-04-15 16:14:24 -0400

    fifth commit

--- myKitchen.js
+++ myKitchen.js
@@ -21,29 +21,134 @@ dairyAlt.forEach(item => {
    dairyItems.append(li);
 });             
 
-// create variables to stored dairy data ids
-var addItem = document.querySelector(".btn");
-   var savedDairyItems = [];
-   console.log(savedDairyItems)
+var produce = [["asparagus spears", "11011"],
+            [""]
+];
+var produceAlt = [ ["acorn squash", "11482"],
+               ["avocado", "9037"],
+               ["baby bell peppers", "10311821"],
+               ["broccoli", "11090"],
+               ["brussel sprouts" , "11098"],
+               ["carrots" , "11124"]
+];
+produceItems = $("#produceItems")
+produceAlt.forEach(item => {
+   var li = $(`<li>${item[0]}</li>`)
+   li.attr(`data-id`,item[1])
+   var button = $(`<button type = button class = "btn btn-secondary">Add Item</button>`);
+   li.append(button);
+   produceItems.append(li);
+}); 
 
-   var dairy = dairyAlt;
+var meat = [["beef brisket" , "13023"],
+            [""]
+];
+var meatAlt = [ ["beef chuck roast" , "13786"],
+               ["beef tenderloin" , "13926"],
+               ["boneless skinless chicken breast" , "1055062"],
+               ["chicken sausage" , "93668"],
+               ["lean pork tenderloin" , "10060"],
+               ["pork chops" , "10010062"]
+];
+meatItems = $("#meatItems")
+meatAlt.forEach(item => {
+   var li = $(`<li>${item[0]}</li>`)
+   li.attr(`data-id`,item[1])
+   var button = $(`<button type = button class = "btn btn-secondary">Add Item</button>`);
+   li.append(button);
+   meatItems.append(li);
+});  
 
-   savedDairyItems.push(dairyAlt);
+
+var seafood = [["cat fish filets" , "15010"],
+            [""]
+];
+var seafoodAlt = [ ["raw shrimp" , "15152"],
+               ["crabmeat" , "10015136"],
+               ["clams", "15157"],
+               ["mussels" , "15164"],
+               ["salmon fillet" , "15076"],
+               ["sea scallops" , "10015172"]
+];
+seafoodItems = $("#seafoodItems")
+seafoodAlt.forEach(item => {
+   var li = $(`<li>${item[0]}</li>`)
+   li.attr(`data-id`,item[1])
+   var button = $(`<button type = button class = "btn btn-secondary">Add Item</button>`);
+   li.append(button);
+   seafoodItems.append(li);
+}); 
+
+var pantry = [["cat fish filets" , "15010"],
+            [""]
+];
+var pantryAlt = [ ["fettuccine" , "100204092"],
+               ["spaghetti" , "11420420"],
+               ["rotini pasta" , "11320420"],
+               ["short grain rice" , "10120052"],
+               ["extra virgin olive oil:" , "1034053"],
+               ["flour" , "20081"]
+];
+pantryItems = $("#pantryItems")
+pantryAlt.forEach(item => {
+   var li = $(`<li>${item[0]}</li>`)
+   li.attr(`data-id`,item[1])
+   var button = $(`<button type = button class = "btn btn-secondary">Add Item</button>`);
+   li.append(button);
+   pantryItems.append(li);
+});  
+
+// create variables to stored ingredients' data and ids in local storage 
+
+var savedItems = [];
+   console.log(savedItems)
+
+   var dairyIngredients = dairyAlt;
+
+   savedItems.push(dairyAlt);
    localStorage.setItem("dairyAlt",JSON.stringify(dairyAlt));
    var storedDairy = JSON.parse(localStorage.getItem("dairyAlt"));
    console.log(storedDairy)
   
+   var produceIngredients = produceAlt;
 
-// need a fuction for when user add an item we need to save it to the local storage 
+   savedItems.push(produceAlt);
+   localStorage.setItem("produceAlt",JSON.stringify(produceAlt));
+   var storedProduce = JSON.parse(localStorage.getItem("produceAlt"));
+   console.log(storedProduce)
+
+   var meatIngredients = meatAlt;
+
+   savedItems.push(meatAlt);
+   localStorage.setItem("meatAlt",JSON.stringify(meatAlt));
+   var storedMeat = JSON.parse(localStorage.getItem("meatAlt"));
+   console.log(storedMeat)
+
+
+   var seafoodIngredients = seafoodAlt;
+
+   savedItems.push(seafoodAlt);
+   localStorage.setItem("seafoodAlt",JSON.stringify(seafoodAlt));
+   var storedSeafood = JSON.parse(localStorage.getItem("seafoodAlt"));
+   console.log(storedSeafood)
+
+   var pantryIngredients = pantryAlt;
+
+   savedItems.push(pantryAlt);
+   localStorage.setItem("pantryAlt",JSON.stringify(pantryAlt));
+   var storedPantry = JSON.parse(localStorage.getItem("pantryAlt"));
+   console.log(storedPantry)
+
+
+renderMealIngredients()
+   // need a fuction for when user add an item we need to retrieve it from the local storage append it to ingredients section 
+   var addItem = document.querySelector(".btn");
+   var mealIngredients = 
    addItem.addEventListener("click", function(event){
       console.log(addItem)
       event.preventDefault();
      
      
-      savedDairyItems.push(dairyAlt);
-      localStorage.setItem("dairyAlt",JSON.stringify(dairyAlt));
-      var storedDairy = JSON.parse(localStorage.getItem("dairyAlt"));
-      console.log(storedDairy)
  
 })
 
